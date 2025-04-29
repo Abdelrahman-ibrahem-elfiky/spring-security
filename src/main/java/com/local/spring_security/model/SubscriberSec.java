fbf69b945b02dc160bb96ed22fd3bf52ea57f109
abdelrahman elfiky <abdelrahmanelfiky05@gmail.com>
2025-04-29 12:33:13 +0200

    create cusome Spring Security users

---
+++ src/main/java/com/local/spring_security/model/SubscriberSec.java
@@ -0,0 +1,38 @@
+package com.local.spring_security.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class SubscriberSec implements UserDetails {
+
+
+    private subscriber subscriber;
+
+    public SubscriberSec(com.local.spring_security.model.subscriber subscriber) {
+        this.subscriber = subscriber;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        List<GrantedAuthority>authorities=new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(subscriber.getRole()));
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return subscriber.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return subscriber.getEmail();
+    }
+
+}
